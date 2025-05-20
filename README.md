fd1392fc52321fed86519109a29adf299d872491
bain-liao-1 <liao.bain@stu.ocsb.ca>
2025-05-19 21:03:35 -0400

    created 6-02 extra

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-# ICS2O ⇢ Unit #6-02
+# ICS2O ⇢ Unit #6-02-EXTRA
 
-[![Mr Coxall's Super Linter](https://github.com/<OWNER>/<REPOSITORY>/workflows/Mr%20Coxall's%20Super%20Linter/badge.svg)](https://github.com/<OWNER>/<REPOSITORY>/actions)
+[![Mr Coxall's Super Linter](https://github.com/bain-liao-1/ICS2O-Unit-6-02-EXTRA-bain-liao-1/workflows/Mr%20Coxall's%20Super%20Linter/badge.svg)](https://github.com/bain-liao-1/ICS2O-Unit-6-02-EXTRA-bain-liao-1/actions)
 
-[![Deploy Pages](https://github.com/<OWNER>/<REPOSITORY>/workflows/Deploy%20Pages/badge.svg)](https://github.com/<OWNER>/<REPOSITORY>/actions)
+[![Deploy Pages](https://github.com/bain-liao-1/ICS2O-Unit-6-02-EXTRA-bain-liao-1/workflows/Deploy%20Pages/badge.svg)](https://github.com/bain-liao-1/ICS2O-Unit-6-02-EXTRA-bain-liao-1/actions)
 
-This site can be found at: [https://<OWNER>.github.io/<REPOSITORY>](https://<OWNER>.github.io/<REPOSITORY>)
+This site can be found at: [https://bain-liao-1.github.io/ICS2O-Unit-6-02-EXTRA-bain-liao-1](https://bain-liao-1.github.io/ICS2O-Unit-6-02-EXTRA-bain-liao-1)
 
 ---
 
--- js/script.js
+++ js/script.js
@@ -1,5 +1,43 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Bain Liao
+// Created on: May 2025
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * Check service worker.
+ */
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/ICD2O-Unit-6-02-EXTRA-bain-liao-1/sw.js", {
+    scope: "/ICD2O-Unit-6-02-EXTRA-bain-liao-1/",
+  })
+}
+
+let userAge = sessionStorage.getItem('userAge') ? Number(sessionStorage.getItem('userAge')) : null
+
+/**
+ * This function checks and updates the user's age in sessionStorage.
+ */
+function ageCheck() {
+  if (userAge === null) {
+    userAge = parseInt(document.getElementById('user-age').value)
+    sessionStorage.setItem('userAge', userAge)
+  }
+
+  // Check the user's age
+  if (userAge >= 17) {
+    document.getElementById('result').innerHTML =
+      '<p>You can go see an R rated movie alone.</p>'
+  } else if (userAge >= 13) {
+    document.getElementById('result').innerHTML =
+      '<p>You can go see a PG-13 movie alone.</p>'
+  } else if (userAge >= 5) {
+    document.getElementById('result').innerHTML =
+      '<p>You can go see a G or PG movie alone.</p>'
+  } else {
+    document.getElementById('result').innerHTML =
+      '<p>You are too young to see most things.</p>'
+  }
+}
